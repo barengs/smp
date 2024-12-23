5a5ab5bca9eca4ec3000f43acde945d7f7aecf28
Luqmanul Hakiem <luqmanul@ebconnect.com>
2024-12-23 09:40:32 +0700

    feat: crud non formal education

--- app/Http/Controllers/Api/NonFormalEducationController.php
+++ app/Http/Controllers/Api/NonFormalEducationController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\EducationRequest;
+use App\Http\Resources\ApiResource;
+use App\Models\NonFormalEducation;
 use Illuminate\Http\Request;
 
 class NonFormalEducationController extends Controller
@@ -12,54 +15,56 @@ class NonFormalEducationController extends Controller
      */
     public function index()
     {
-        //
+        $data = NonFormalEducation::get();
+        return new ApiResource(true, 'List data Pendidikan Non Formal', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(EducationRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        $nonFormalEducation = NonFormalEducation::create($data);
+
+        return new ApiResource(true, 'Berhasil menambahkan Pendidikan Non Formal', $nonFormalEducation);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $data = NonFormalEducation::findorfail($id);
+
+        return new ApiResource(true, 'Data Pendidikan Non Formal', $data);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(EducationRequest $request, string $id)
     {
-        //
+        $data = $request->validated();
+
+        $find = NonFormalEducation::findorfail($id);
+
+        if ($find) {
+            $new = $find->update($data);
+            return new ApiResource(true, 'Berhasil mengupdate  Pendidikan Non Formal', $find);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $find = NonFormalEducation::findorfail($id);
+        $find->delete();
+        return new ApiResource(true, 'Berhasil mendelete Pendidikan Non Formal', $find);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AttendantController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FormalEducationController;
+use App\Http\Controllers\Api\NonFormalEducationController;
 use App\Http\Controllers\Api\ParentProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,4 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('karyawan', AttendantController::class);
 Route::apiResource('orang-tua', ParentProfileController::class);
 Route::apiResource('formal-education', FormalEducationController::class);
+Route::apiResource('nonformal-education', NonFormalEducationController::class);
